f60a48f316054de99972e819b5ff567485bcac90
g <157315309+doesdesignwork@users.noreply.github.com>
2026-07-19 20:22:02 +0800

    Use custom domain for canonical URLs

--- lib/site.ts
+++ lib/site.ts
@@ -1,8 +1,6 @@
-const productionHost = process.env.VERCEL_PROJECT_PRODUCTION_URL;
-
 export const siteUrl = (
   process.env.NEXT_PUBLIC_SITE_URL ??
-  (productionHost ? `https://${productionHost}` : "http://localhost:3000")
+  "https://doesdesignwork.com"
 ).replace(/\/$/, "");
 
 export const lastModified = "2026-07-19";
