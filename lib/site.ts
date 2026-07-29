90a6c82992853c220dcbe6a50096471cfd0a2f40
g <157315309+doesdesignwork@users.noreply.github.com>
2026-07-29 19:23:55 +0800

    Fix structured data modification datetime (#23)
    Use a complete ISO 8601 datetime with Singapore timezone information for every JSON-LD dateModified field.

--- lib/site.ts
+++ lib/site.ts
@@ -3,4 +3,4 @@ export const siteUrl = (
   "https://www.doesdesignwork.com"
 ).replace(/\/$/, "");
 
-export const lastModified = "2026-07-23";
+export const lastModified = "2026-07-29T19:22:11+08:00";
