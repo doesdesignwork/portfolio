a19427b89f3ec9a7f0e5e11c7e79b1c3053ad796
g <157315309+doesdesignwork@users.noreply.github.com>
2026-07-30 00:22:04 +0800

    Add case studies and services to sitemap

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,4 +1,5 @@
 import { projects } from "../data/projects";
+import { services } from "../data/services";
 import { lastModified, siteUrl } from "@/lib/site";
 
 const escapeXml = (value: string) =>
@@ -9,26 +10,52 @@ const escapeXml = (value: string) =>
     .replace(/\"/g, "&quot;")
     .replace(/'/g, "&apos;");
 
-const projectImages = Array.from(
-  new Set(projects.flatMap((project) => project.images)),
-);
-
-const imageEntries = projectImages
-  .map(
-    (image) => `    <image:image>
+const imageEntries = (images: string[]) =>
+  images
+    .map(
+      (image) => `    <image:image>
       <image:loc>${escapeXml(`${siteUrl}${image}`)}</image:loc>
     </image:image>`,
+    )
+    .join("\n");
+
+const staticUrls = [
+  { path: "/", priority: "1.0", changefreq: "monthly" },
+  ...services.map((service) => ({
+    path: `/services/${service.slug}/`,
+    priority: "0.8",
+    changefreq: "monthly",
+  })),
+];
+
+const staticEntries = staticUrls
+  .map(
+    (entry) => `  <url>
+    <loc>${escapeXml(`${siteUrl}${entry.path}`)}</loc>
+    <lastmod>${lastModified}</lastmod>
+    <changefreq>${entry.changefreq}</changefreq>
+    <priority>${entry.priority}</priority>
+  </url>`,
+  )
+  .join("\n");
+
+const projectEntries = projects
+  .map(
+    (project) => `  <url>
+    <loc>${escapeXml(`${siteUrl}/work/${project.slug}/`)}</loc>
+    <lastmod>${lastModified}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>${project.featured ? "0.9" : "0.7"}</priority>
+${imageEntries(project.images)}
+  </url>`,
   )
   .join("\n");
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
   xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
-  <url>
-    <loc>${siteUrl}/</loc>
-    <lastmod>${lastModified}</lastmod>
-${imageEntries}
-  </url>
+${staticEntries}
+${projectEntries}
 </urlset>`;
 
 export function GET() {
