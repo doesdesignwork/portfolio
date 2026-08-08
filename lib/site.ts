837c4af5a2a8ceb56db53859124464a56d0831c4
g <157315309+doesdesignwork@users.noreply.github.com>
2026-08-08 16:32:51 +0800

    Update portfolio modified date

--- lib/site.ts
+++ lib/site.ts
@@ -3,4 +3,4 @@ export const siteUrl = (
   "https://www.doesdesignwork.com"
 ).replace(/\/$/, "");
 
-export const lastModified = "2026-08-04T00:00:00+08:00";
+export const lastModified = "2026-08-08T16:11:00+08:00";
